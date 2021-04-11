a27df06052a0ecc1522395081661807c176b7080
Kanu <kanu10@live.com.au>
2021-04-11 16:11:22 +1000

    Updated two-patch model for larval dispersal

--- two_patch.m
+++ two_patch.m
@@ -2,21 +2,19 @@
 clear all
 
 % Define problem parameters
-alpha_x = 0.05;         % interaction constant in equation for coral
+alpha_x = 0.1;         % interaction constant in equation for coral
 alpha_y = 0.01;         % interaction constant in equation for COTS
-alpha_s = 1;            % constant in COTS survival term
-beta_s = 1;             % constant in COTS survival term      
+alpha_S = 1;            % constant in COTS survival term
+beta_S = 1;             % constant in COTS survival term      
 num_reefs = 2;          % number of reefs      
 
 % Initialise time vector
 t_0 = 0;
 t_start = 1;
-t_end = 20;
+t_end = 20;                                 % time in years
 t_vec = t_0:1:t_end;
-
 % Initialise a vector for each reef area
-A_0 = [100; 70];
-A = A_0 * ones(1, length(t_vec));              % reef area
+A = [100; 70];                              % reef area
 
 % Initialise matrices for coral and COTS
 x = zeros(num_reefs, length(t_vec));        % coral cover
@@ -27,15 +25,21 @@ x(:, 1) = x_0;
 y(:, 1) = y_0;
 
 % Initialise matrix for control effort
-k_0 = 0.5; 
-k = k_0 * ones(num_reefs, length(t_vec)-1);   % control effort
+k_0 = [0.3; 0.3]; 
+k = k_0 * ones(1, length(t_vec)-1); % control effort
 
-% Initialise matrices for larval recruitment and survival
-R_0 = [3; 2];
-R = R_0 * ones(1, length(t_vec));           % coral larval recruitment
-S_0 = [10; 4];
-S = S_0 * ones(1, length(t_vec));           % COTS larval recruitment
-phi = zeros(num_reefs, length(t_vec));      % coral larval survival 
+% Initialise matrices for larval recruitment and survival for coral
+r_x = 1;
+c_x = [0,   1;                              % coral larval dispersal
+       0.5, 0.5];       
+R = zeros(num_reefs, length(t_vec));        % coral larval recruitment
+phi = zeros(num_reefs, length(t_vec));      % actual larval recruitment
+
+% Initialise matrices for larval recruitment for COTS
+r_y = 1;
+c_y_0 = [20; 10];
+c_y = c_y_0 * ones(1, length(t_vec));       % COTS larval dispersal
+S = zeros(num_reefs, length(t_vec));        % COTS larval recruitment
 
 
 %% Solve
@@ -43,24 +47,36 @@ phi = zeros(num_reefs, length(t_vec));      % coral larval survival
 for t = t_start:t_end
     % Loop over each reef
     for i = 1:num_reefs
+        % Calculate COTS larval recruitment
+        sum_S = 0;
+        for j = 1:num_reefs
+            sum_S = sum_S + c_y(j, i) * y(j, t) * A(j) * r_y;
+        end
+        S(i, t+1) = 1/A(i) * sum_S;
+        
+        % Calculate coral larval recuitment
+        sum_R = 0;
+        for j = 1:num_reefs
+            sum_R = sum_R + c_x(j, i) * x(j, t) * A(j) * r_x;
+        end
+        R(i, t+1) = 1/A(i) * sum_R;        
+        
         % Calculate coral settlement based on area left on reef
-        if R(i, t) <= (A(i) - x(i, t))
-            phi(i, t+1) = R(i, t);
+        if R(i, t+1) <= (A(i) - x(i, t))
+            phi(i, t+1) = R(i, t+1);
         else
             phi(i, t+1) = A(i) - x(i, t);
         end
         
         % Decide on control effort 
-        if y(i, t) < 10
-            k(i, t) = 0.3;
-        elseif y(i, t) < 5
+        if y(i, t) < 5
             k(i, t) = 0;
         end
         
         % Calculate the population for the next year
         x(i, t+1) = x(i, t) * (1 - alpha_x * y(i, t)) + phi(i, t+1);
         y(i, t+1) = y(i, t) * (1 - alpha_y * x(i, t) - k(i, t)) ...
-                        + (alpha_s * S(i, t+1))/(1 + beta_s * S(i, t+1));
+                        + (alpha_S * S(i, t+1))/(1 + beta_S * S(i, t+1));
         
         % Check they aren't less than zero
         if x(i, t+1) < 0
@@ -79,7 +95,6 @@ for i = 1:num_reefs
     subplot(1, num_reefs, i), hold on
     plot(t_vec, x(i, :), '-o', 'Linewidth', 1.5)
     plot(t_vec, y(i, :), '-o', 'Linewidth', 1.5)
-    ylim([0 40])
     xlabel('Time (in years)', 'Interpreter', 'Latex', 'Fontsize', 12)
     ylabel('Population Size', 'Interpreter', 'Latex', 'Fontsize', 12)
     title(['Reef ', num2str(i)], 'Interpreter', 'Latex', 'Fontsize', 13)
