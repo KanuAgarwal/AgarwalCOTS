c33c1118729dc0e6d5d38a99e2682bb48fad0e2f
Kanu <kanu10@live.com.au>
2021-04-27 01:01:24 +1000

    Updated model to account for mortality then reproduction

--- two_patch.m
+++ two_patch.m
@@ -3,25 +3,29 @@ clear all
 
 % Define problem parameters
 alpha_x = 1;            % growth rate of coral 
-beta_x = 0;         % mortality rate of coral from starfish
-alpha_y = 0;            % natural mortality rate of starfish
-alpha_S = 1;            % constant in starfish survival term
-beta_S = 1;             % constant in starfish survival term      
+beta_x = 0.03;          % mortality rate of coral from starfish
+alpha_y = 0.05;         % starfish response to coral
+beta_y = 0.01;          % natural mortality rate of starfish
+alpha_S = 1;            % constant in starfish larval survival term
+beta_S = 1;             % constant in starfish larval survival term      
 num_reefs = 2;          % number of reefs      
 
 % Initialise time vector
 t_0 = 0;
 t_start = 1;
-t_end = 10;                                 % time in years
+t_end = 20;                                 % time in years
 t_vec = t_0:1:t_end;
+
 % Initialise a vector for each reef area
 A = [100; 70];                              % reef area
 
 % Initialise matrices for coral and starfish
-x = zeros(num_reefs, length(t_vec));        % coral cover
-y = zeros(num_reefs, length(t_vec));        % starfish
+x = zeros(num_reefs, length(t_vec));        % coral cover each year
+x_end = zeros(num_reefs, length(t_vec)-1);  % coral cover at end of year
+y = zeros(num_reefs, length(t_vec));        % starfish each year
+y_end = zeros(num_reefs, length(t_vec)-1);  % starfish at end of year
 x_0 = [20; 30];                             % initial coral cover
-y_0 = [15; 25];                             % initial starfish 
+y_0 = [30; 25];                             % initial starfish 
 x(:, 1) = x_0;
 y(:, 1) = y_0;
 
@@ -30,7 +34,7 @@ k_0 = [0; 0];
 k = zeros(num_reefs, length(t_vec)-1);      % control effort
 
 % Larval recruitment and survival for coral
-r_x = 0;                                    % production rate
+r_x = 5;                                    % production rate
 c_x = [0,   0.2;
        0.2, 0.1];                           % coral larval dispersal
 % We need to account for the percentage that die by floating off
@@ -39,9 +43,9 @@ R = zeros(num_reefs, length(t_vec));        % coral larval recruitment
 phi = zeros(num_reefs, length(t_vec));      % actual larval recruitment
 
 % Larval recruitment for starfish
-r_y = 2;                                    % production rate
-c_y = [0, 0.1;
-       0.1, 0];                           % starfish larval dispersal
+r_y = 15;                                    % production rate
+c_y = [0.1, 0.4;
+       0.5, 0.1];                           % starfish larval dispersal
 % We need to account for the percentage that die by floating off
 c_y_dead = 1 - sum(c_y, 2); 
 S = zeros(num_reefs, length(t_vec));        % starfish larval recruitment
@@ -50,7 +54,14 @@ S = zeros(num_reefs, length(t_vec));        % starfish larval recruitment
 %% Solve
 % Loop over and calculate population size through time
 for t = t_start:t_end
-    % Loop over each reef
+    % Loop over each reef and calculate population at the end of last year
+    for i = 1:num_reefs
+        x_end(i, t) = x(i, t) * (alpha_x - beta_x * y(i, t));
+        y_end(i, t) = y(i, t) * (alpha_y * x(i, t) - beta_y) - k(i, t);
+    end
+    
+    % Loop over each reef and calculate population this year based on last
+    % year
     for i = 1:num_reefs
         % Calculate starfish larval recruitment
         for j = 1:num_reefs
@@ -69,18 +80,13 @@ for t = t_start:t_end
             phi(i, t+1) = A(i) - x(i, t);
         end
         
-%         % Decide on control effort 
-%         if y(i, t) > 5
-%             k(i, t) = k_0(i);
-%         end
-        
         % Calculate the population for the next year
-        x(i, t+1) = x(i, t) * (alpha_x - beta_x * y(i, t)) ...
-                        + phi(i, t+1);
-        y(i, t+1) = y(i, t) * (1 - alpha_y) - k(i, t) ...
-                        + (alpha_S * S(i, t+1))/(1 + beta_S * S(i, t+1));
+        x(i, t+1) = x_end(i, t) + phi(i, t+1);
+        y(i, t+1) = y_end(i, t) + (alpha_S * S(i, t+1))/(1 + beta_S * S(i, t+1));
+        % Round number of starfish to nearest integer
+        y(i, t+1) = round(y(i, t+1));
         
-        % Check they aren't less than zero
+        % Check coral cover and no. of starfish isn't less than zero
         if x(i, t+1) < 0
             x(i, t+1) = 0;
         end
@@ -100,6 +106,6 @@ for i = 1:num_reefs
     xlabel('Time (in years)', 'Interpreter', 'Latex', 'Fontsize', 12)
     ylabel('Population Size', 'Interpreter', 'Latex', 'Fontsize', 12)
     title(['Reef ', num2str(i)], 'Interpreter', 'Latex', 'Fontsize', 13)
-    legend('Coral Cover (sqm)', 'Starfish (no.)', 'Location', 'SouthWest')
-    ylim([0 ceil(max(max(x(i, :), y(i, :))))])
+    legend('Coral Cover (sqm)', 'Starfish (no.)')
+%     ylim([0 ceil(max(max(x(i, :), y(i, :))))])
 end
