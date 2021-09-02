f2a1def39057e076fdefb464ae66a73fede625ef
KanuAgarwal <kanu10@live.com.au>
2021-09-02 21:40:08 +1000

    Coded up the model from the Morello paper

---
+++ simulate_reefs_v2.m
@@ -0,0 +1,176 @@
+function [t_vec, C_y_f, C_y_m, N_y_2, N_y_1, N_y_0] = simulate_reefs_v2(num_reefs, t_end, params, initial_state, control_effort)
+
+% This function simulates starfish and coral populations at n reefs for t 
+% years. 
+%
+% The function inputs are:
+%   num_reefs      = number of reefs to simulate
+%   t_end          = final timestep i.e. number of years to run simlation
+%   params         = a struct with parameter values for model equations
+%   intial_state   = a struct with the initial state of the system i.e. the 
+%                    initial coral cover and starfish populations
+%   control_effort = an matrix (size n x t-1) with control effort at each
+%                    reef over time, or a constant for constant or no
+%                    control effort 
+%
+% The function outputs are:
+%   t_vec = a vector of time (in years) that the simulation is run over
+%   x     = a matrix of coral cover for each reef over time
+%   y     = a matrix of starfish poopulations for each reef over time
+
+
+% PARAMETERS ==============================================================
+% Parameters in model equations
+p_tilde = params.p_tilde;       % effect of fast-growing coral on COTS     
+M_cots = params.M_cots;         % natural mortality of COTS 
+p_1_f = params.p_1_f;           % effect of COTS on fast-growing coral
+p_1_m = params.p_1_m;           % effect of COTS on slow-growing coral
+r_f = params.r_f;               % intrinsic growth rate of fast-growing coral
+r_m = params.r_m;               % intrinsic growth rate of slow-growing coral
+K_f = params.K_f;               % carrying capacity of fast-growing coral
+K_m = params.K_m;               % carrying capacity of slow-growing coral
+p_2_f = params.p_2_f;           % effect of COTS on fast-growing coral
+p_2_m = params.p_2_m;           % effect of COTS on slow-growing coral
+% rho = params.rho;               % proliferation rate of starfish larvae 
+% K = params.K;                   % starfish larvae carrying capacity
+% r_c = params.r_c;               % coral larvae production rate
+% r_s = params.r_s;               % starfish larvae production rate
+% kappa_c = params.kappa_c;       % coral larvae connectivity matrix
+% kappa_s = params.kappa_s;       % starfish larvae connectivity matrix
+% A = params.A;                   % reef area i.e. coral carrying capacity
+
+% Initial state of the system
+C_0_f = initial_state.C_0_f;            % fast-growing coral
+C_0_m = initial_state.C_0_m;            % slow-growing coral
+N_0_2 = initial_state.N_0_2;            % age 2+ COTS
+N_0_1 = initial_state.N_0_1;            % age 1 COTS
+N_0_0 = initial_state.N_0_0;            % age 0 COTS
+
+
+% SETUP ===================================================================
+% Initialise time vector
+t_0 = 0;
+t_vec = t_0:1:t_end;
+
+% Initialise matrices for coral and starfish
+C_y_f = zeros(num_reefs, length(t_vec));
+C_y_m = zeros(num_reefs, length(t_vec));
+N_y_2 = zeros(num_reefs, length(t_vec));
+N_y_1 = zeros(num_reefs, length(t_vec));
+N_y_0 = zeros(num_reefs, length(t_vec));
+C_y_f(:, 1) = C_0_f;
+C_y_m(:, 1) = C_0_m;
+N_y_2(:, 1) = N_0_2;
+N_y_1(:, 1) = N_0_1;
+N_y_0(:, 1) = N_0_0;
+
+% Matrix for control effort
+% % If there is no control effort, create a zero  matrix
+% if control_effort == 0
+%     k = zeros(num_reefs, length(t_vec)-1);
+% % Otherwise, leave the control effort matrix as is
+% else
+%     k = control_effort;
+% end
+
+% Larval recruitment
+% % Keep track of percentage of larvae that die at each reef
+% kappa_c_dead = 1 - sum(kappa_c, 2);
+% kappa_s_dead = 1 - sum(kappa_s, 2);
+% % Matrices for larval recruitment before mortality
+% sigma = zeros(num_reefs, length(t_vec)-1);              % coral
+% tau = zeros(num_reefs, length(t_vec)-1);                % starfish
+% % Matrices for larval recruitment after mortality
+% phi = zeros(num_reefs, length(t_vec)-1);                % coral
+% gamma = zeros(num_reefs, length(t_vec)-1);              % starfish
+
+
+% SOLVE ===================================================================
+% Loop over and calculate population size through time
+for t = t_0+1:t_end
+    % Loop over and calculate population at each reef
+    for i = 1:num_reefs
+        
+%         % Calculate starfish larval recruitment
+%         for j = 1:num_reefs
+%             tau(i, t) = tau(i, t) + kappa_s(j, i) * y(j, t) * r_s;
+%         end
+% 
+%         % Use Beverton-Holt model for starfish survival
+%         gamma(i, t) = (rho * tau(i, t)) / (1 + (rho - 1)/K(i) * tau(i, t));
+%         
+%         % Check gamma isn't negative
+%         if gamma(i, t) < 0
+%             gamma(i, t) = 0;
+%         end
+%        
+%         % Calculate coral larval recuitment
+%         for j = 1:num_reefs
+%             sigma(i, t) = sigma(i, t) + kappa_c(j, i) * x(j, t) * r_c;
+%         end
+%         
+%         % Calculate coral settlement based on area left on reef
+%         if sigma(i, t) <= (A(i) - x(i, t))
+%             phi(i, t) = sigma(i, t);
+%         else
+%             phi(i, t) = A(i) - x(i, t);
+%         end
+
+        % Calculate population sizes for age 0 COTS depending on year -
+        % based on Morello paper - testing base case model
+        if t == 1
+            N_y_0(i, t+1) = 1 + exp(4.292);
+        elseif t == 3
+            N_y_0(i, t+1) = exp(4.307) + 1;
+        else
+            N_y_0(i, t+1) = 2;
+        end
+        
+        % Functions to help calculate age 1 and 2+ COTS population sizes
+        f_of_C = 1 - p_tilde * (C_y_f(i, t) / (1 + C_y_f(i, t)));
+
+        % Calculate population sizes for age 1 and 2+ COTS
+        N_y_1(i, t+1) = N_y_0(i, t) * exp(-f_of_C * M_cots);
+        N_y_2(i, t+1) = (N_y_1(i, t) + N_y_2(i, t)) * exp(-f_of_C * M_cots);
+        
+        % Function for calculating different coral population sizes
+        rho_y = 1 + exp(-5 * C_y_f(i, t) / K_f);
+        
+        % Fast-growing coral mortality from COTS 
+        Q_y_f = (1-rho_y) * (p_1_f * (N_y_1(i, t) + N_y_2(i, t)) * C_y_f(i, t)) ...
+                    / (1 + exp( -(N_y_1(i, t) + N_y_2(i, t)) / p_2_f));
+
+        % Slow-growing coral mortality from COTS
+        Q_y_m = rho_y * (p_1_m * (N_y_1(i, t) + N_y_2(i, t)) * C_y_m(i, t)) ...
+                    / (1 + exp( -(N_y_1(i, t) + N_y_2(i, t))/p_2_m) );
+                
+        % Calculate population size for fast-growing coral
+        C_y_f(i, t+1) = C_y_f(i, t) + r_f * C_y_f(i, t) * (1 - C_y_f(i, t)/K_f) - Q_y_f;
+        
+        % Calculate population size for slow-growing coral
+        C_y_m(i, t+1) = C_y_m(i, t) + r_m * C_y_m(i, t) * (1 - C_y_m(i, t)/K_m) - Q_y_m;
+        
+        % Ensure populations aren't less than zero
+        if C_y_f(i, t+1) < 0
+            C_y_f(i, t+1) = 0;          % fast-growing coral
+        end
+
+        if C_y_m(i, t+1) < 0
+            C_y_m(i, t+1) = 0;          % slow-growing coral
+        end
+        
+        if N_y_0(i, t+1) < 0
+            N_y_0(i, t+1) = 0;          % age 0 COTS
+        end
+        
+        if N_y_1(i, t+1) < 0
+            N_y_1(i, t+1) = 0;          % age 1 COTS
+        end
+        
+        if N_y_2(i, t+1) < 0            % age 2+ COTS
+            N_y_2(i, t+1) = 0;
+        end
+    end
+end
+
+% END =====================================================================
---
+++ simulations_v2_morello.m
@@ -0,0 +1,101 @@
+clear all
+
+% PARAMETERISATION
+
+% Get connectivity matrices
+% load IdentifyKeySources/ConnectivityMatrices_Model_A_2002_P7
+
+% PARAMETERS
+num_reefs = 2;                  % number of reefs 
+t_end = 15;                     % time in years
+control_effort = 0;             % no control effort
+
+% Parameters struct: store all parameters in a struct
+% Constant parameter values for all reefs
+% Estimated by Morello et al. (2014)
+params.p_tilde = 0.258;         % effect of fast-growing coral on COTS     
+params.M_cots = 2.56;           % natural mortality of COTS 
+params.p_1_f = 0.129;           % effect of COTS on fast-growing coral
+params.p_1_m = 0.268;           % effect of COTS on slow-growing coral
+
+% Known or arbitrarily chosen by Morello et al. (2014)
+params.r_f = 0.5;               % intrinsic growth rate of fast-growing coral
+params.r_m = 0.1;               % intrinsic growth rate of slow-growing coral
+params.K_f = 2500;              % carrying capacity of fast-growing coral
+params.K_m = 500;               % carrying capacity of slow-growing coral
+params.p_2_f = 10;              % effect of COTS on fast-growing coral
+params.p_2_m = 8;               % effect of COTS on slow-growing coral
+
+% % Known or arbitrarily chosen by me
+% params.rho = 1;                 % proliferation rate of starfish larvae 
+% params.K = 1;                   % starfish larvae carrying capacity
+% params.r_c = 1;                 % coral larvae production rate
+% params.r_s = 1;                 % starfish larvae production rate
+% 
+% % Reef dependent variables
+% params.kappa_c = 1;             % coral larvae connectivity matrix
+% params.kappa_s = 1;             % starfish larvae connectivity matrix
+% params.A = 1;                   % reef area i.e. coral carrying capacity
+
+% Initial system state
+% initial biomass of fast-growing coral = carrying capacity
+initial_state.C_0_f = params.K_f * ones(num_reefs, 1);      
+% initial_state.C_0_f = 0 * ones(num_reefs, 1);      
+% initial biomass of slow-growing coral = carrying capacity
+initial_state.C_0_m = params.K_m * ones(num_reefs, 1);  
+% initial_state.C_0_m = 0 * ones(num_reefs, 1);  
+% initial number of COTS aged 2+ = estimated
+initial_state.N_0_2 = 0.505 * ones(num_reefs, 1);   
+% initial number of COTS aged 1 = no. of age 2+ COTS * function
+initial_state.N_0_1 = initial_state.N_0_2 * exp(params.M_cots);
+% initial number of COTS aged 0 = no. of age 2+ COTS * function
+initial_state.N_0_0 = initial_state.N_0_2 * exp(2*params.M_cots);
+
+
+% SOLVE
+[t_vec, C_y_f, C_y_m, N_y_2, N_y_1, N_y_0] = simulate_reefs_v2(num_reefs, t_end, params, initial_state, control_effort);
+
+% Calculate coral cover from coral biomass
+C_y_f_cover = C_y_f.^(2/3);
+C_y_m_cover = C_y_m.^(2/3);
+
+% PLOT
+% Coral biomass
+figure(1), clf, hold on
+plot(t_vec, C_y_f(1, :), 'Linewidth', 2)
+plot(t_vec, C_y_m(1, :), 'Linewidth', 2)
+xlabel('Time (years)')
+ylabel('Coral biomass')
+title('Coral biomass at reef 1')
+legend('Fast-growing coral', 'Slow-growing coral')
+
+% Fast-growing coral cover
+figure(2), clf, hold on
+plot(t_vec, C_y_f_cover(1, :), 'Linewidth', 2)
+xlabel('Time (years)')
+ylabel('Coral cover')
+title('Fast-growing coral cover at reef 1')
+
+% Slow-growing coral cover
+figure(3), clf, hold on
+plot(t_vec, C_y_m_cover(1, :), 'Linewidth', 2)
+xlabel('Time (years)')
+ylabel('Coral cover')
+title('Slow-growing coral cover at reef 1')
+
+% All COTS
+figure(4), clf, hold on
+plot(t_vec, N_y_2(1, :), 'Linewidth', 2)
+plot(t_vec, N_y_1(1, :), 'Linewidth', 2)
+plot(t_vec, N_y_0(1, :), 'Linewidth', 2)
+xlabel('Time (years)')
+ylabel('Number of COTS')
+legend('Age 2+ COTS', 'Age 1 COTS', 'Age 0 COTS')
+title('COTS population at reef 1')
+
+% Age 2 COTS 
+figure(5), clf, hold on
+plot(t_vec, N_y_2(1, :), 'Linewidth', 2)
+xlabel('Time (years)')
+ylabel('Number of COTS')
+title('Age 2+ COTS population at reef 1')
